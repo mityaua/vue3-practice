164193a07915e4d7f90771bc1ed4e2bf7b3e2fe8
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-11-12 21:04:38 +0200

    Added files from 4-lsn

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Button>Click me</Button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from "./components/Button.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Button,
+  },
+};
 </script>
 
 <style>
